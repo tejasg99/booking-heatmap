61dcca0580a078f8f60f9a17a2cb08e22ab6ac7d
Tejas Gawade <tgawade092@gmail.com>
2026-05-07 23:52:30 +0530

    Setup: vite and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className='w-screen h-screen bg-gray-100'>
+        <h1 className='text-2xl font-bold text-blue-600 text-center'>Booking Calendar Heatmap</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
